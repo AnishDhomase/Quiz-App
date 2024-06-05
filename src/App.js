183d0f0040a5b5a1af2a367e2de5eba9ee30c686
AnishDhomase <anishdhomase@gmail.com>
2024-06-05 16:44:40 +0530

    Home page done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,77 @@
+import { useState } from "react";
+
 function App() {
-  return <h1 className="App">App</h1>;
+  const [activePage, setActivePage] = useState("home");
+  function handlePageChange(event) {
+    if (event.target.innerHTML === "Let's Go") setActivePage("custQuiz");
+  }
+  return (
+    <div className="App">
+      {activePage === "home" && (
+        <HomePage>
+          <Button
+            onButtonClick={handlePageChange}
+            bgClr="#012E91"
+            pad="15px 20px"
+          >
+            Let's Go
+          </Button>
+        </HomePage>
+      )}
+      {activePage === "custQuiz" && <CustomizeQuizPage />}
+      {activePage === "quiz" && <QuizPage />}
+    </div>
+  );
+}
+
+function HomePage({ children }) {
+  return (
+    <div className="home">
+      <div className="content">
+        <img src="answer (1).png" alt="logo"></img>
+        {children}
+      </div>
+    </div>
+  );
+}
+function CustomizeQuizPage() {
+  return <div className="custQuiz">CustomizeQuiz Page</div>;
+}
+function QuizPage() {
+  return <div className="quiz">Quiz Page</div>;
+}
+
+function Button({
+  onButtonClick,
+  children,
+  txtSz = "20px",
+  txtWt = "500",
+  bgClr = "#88EEE1",
+  txtClr = "#fff",
+  brdr = "none",
+  btnWidth = "100%",
+  btnHeigth = "auto",
+  brdrRad = "30px",
+  pad = "10px 20px",
+  maxWidth = "300px",
+}) {
+  const btnStyle = {
+    backgroundColor: bgClr,
+    color: txtClr,
+    border: brdr,
+    width: btnWidth,
+    height: btnHeigth,
+    borderRadius: brdrRad,
+    fontSize: txtSz,
+    fontWeight: txtWt,
+    padding: pad,
+    maxWidth: maxWidth,
+  };
+  return (
+    <button onClick={(ev) => onButtonClick(ev)} style={btnStyle}>
+      {children}
+    </button>
+  );
 }
 
 export default App;
